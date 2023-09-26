211a54b6fb93c8e0907a9b05f94ad0e4e67b66e7
timmoh-king <timothykariuki43@gmail.com>
2023-09-26 10:37:30 +0300

    9-getFullBudget.js

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,16 @@
+/*
+* Rewrite getFullBudgetObject to use ES6 method properties in the fullBudget object
+*/
+
+import getBudgetObject from './7-getBudgetObject';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars: () => `$${income}`,
+    getIncomeInEuros: () => `${income} euros`,
+  };
+
+  return fullBudget;
+}
