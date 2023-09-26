aa4033e53ecbd09dac897945f56224a4ab61c576
timmoh-king <timothykariuki43@gmail.com>
2023-09-26 09:45:08 +0300

    0-constants.js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,8 +1,3 @@
-/*
- * function taskFirst to instantiate variables using const
- * function taskNext to instantiate variables using let
- */
-
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
