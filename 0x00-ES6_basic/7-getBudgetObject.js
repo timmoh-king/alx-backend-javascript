8f63397769f785dce99680c3f843d2e19575112e
timmoh-king <timothykariuki43@gmail.com>
2023-09-26 10:00:40 +0300

    7-getBudgetObject.js

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,8 +1,3 @@
-/*
-* Notice how the keys and the variable names are the same?
-* Modify the following function’s budget object to simply use the keyname instead.
-*/
-
 export default function getBudgetObject(income, gdp, capita) {
   const budget = { income, gdp, capita };
 
