4ebc324dba70f2456db893e330c0dc535c3450c9
timmoh-king <timothykariuki43@gmail.com>
2023-09-26 12:12:26 +0300

    10-loops.js

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,23 @@
+/*
+* Rewrite the function appendToEachArrayValue to use ES6’s for...of operator
+* And don’t forget that var is not ES6-friendly.
+*/
+
+// export default function appendToEachArrayValue(array, appendString) {
+//   for (let idx of array) {
+//     const value = array[idx];
+//     idx = appendString + value;
+//   }
+
+//   return array;
+// }
+
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+
+  for (const value of array) {
+    newArray.push(appendString + value);
+  }
+
+  return newArray;
+}
