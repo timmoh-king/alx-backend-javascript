defceacbe5dde71a76b85bde3f2c76f3f4c49b4a
timmoh-king <timothykariuki43@gmail.com>
2023-09-27 11:30:06 +0300

    1-promise.js

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,21 @@
+/*
+* Using the prototype below, return a promise. The parameter is a boolean.
+* When the argument is true:
+* resolve the promise by passing an object with 2 attributes: status: 200 body: 'success'
+* when false:
+* reject the promise with an error object with the message The fake API is not working currently
+*/
+
+export default function getFullResponseFromAPI(success) {
+  const arr = new Promise((resolve, reject) => {
+    if (success === true) {
+      resolve({
+        status: 200,
+        body: 'success',
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+  return arr;
+}
