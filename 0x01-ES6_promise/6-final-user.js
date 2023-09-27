865eb1e683c1ebf58566664524a96d7ca84749bf
timmoh-king <timothykariuki43@gmail.com>
2023-09-27 23:10:32 +0300

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -9,23 +9,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup (firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((response) => {
-      const result = [];
-      response.forEach((promise) => {
-        if (promise.status === 'fulfilled') {
-          result.push({
-            status: promise.status,
-            value: promise.value
-          });
-        } else {
-          result.push({
-            status: promise.status,
-            value: `Error: ${promise.reason.message}`
-          });
-        }
-      });
-      return result;
-    });
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason)
+      }))
+    ));
 }
