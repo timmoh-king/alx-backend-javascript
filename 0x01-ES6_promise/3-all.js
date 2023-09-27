cb634296d88bf1281be3868ceef7e75567808843
timmoh-king <timothykariuki43@gmail.com>
2023-09-27 23:02:14 +0300

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -8,7 +8,7 @@
 
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup () {
+export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((response) => {
       console.log(response[0].body, response[1].firstName, response[1].lastName);
