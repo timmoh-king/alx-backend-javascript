21a586f19d6123da5f59758cd39298829ef991fc
timmoh-king <timothykariuki43@gmail.com>
2023-09-27 23:12:49 +0300

    7-load_balancer.js

---
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -0,0 +1,11 @@
+/*
+ * Write and export a function named loadBalancer
+ * It should accept two arguments chinaDownload (Promise) and USDownload (Promise).
+ * The function should return the value returned by the promise that resolved the first.
+ */
+
+export default function loadBalancer (chinaDownload, USDownload) {
+  return Promise
+    .race([chinaDownload, USDownload])
+    .then((res) => res);
+}
