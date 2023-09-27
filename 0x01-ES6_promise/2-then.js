6ec92dc31ce04a74725114469a0dbc6d4056119f
timmoh-king <timothykariuki43@gmail.com>
2023-09-27 12:05:30 +0300

    2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -8,9 +8,7 @@
  */
 
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({
-    status: 200,
-    body: 'success',
-  }), (() => new Error()))
+  return promise.then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
     .finally(() => console.log('Got a response from the API'));
 }
