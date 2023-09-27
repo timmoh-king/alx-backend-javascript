1a3152bc31e9edd8fb9ee14a42cfa6cb9980876a
timmoh-king <timothykariuki43@gmail.com>
2023-09-27 04:57:06 +0300

    1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -12,7 +12,7 @@ export default function getFullResponseFromAPI(success) {
     if (success) {
       resolve({ status: 200, body: 'success' });
     } else {
-      reject(Error('The fake API is not working currently'));
+      reject(new Error('The fake API is not working currently'));
     }
   });
   return arr;
