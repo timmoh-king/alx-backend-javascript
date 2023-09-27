f29c36480590351d3acd2090fc7d082b168e8e54
timmoh-king <timothykariuki43@gmail.com>
2023-09-27 11:54:28 +0300

    2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,19 +1,17 @@
 /*
-* Using the function prototype below: function handleResponseFromAPI(promise)
-* Append three handlers to the function
-* When the Promise resolves, return an object with the following attributes:
-* status: 200 body: 'success'
-* When the Promise rejects, return an empty Error object
-* For every resolution, log Got a response from the API to the console
-*/
+ * Using the function prototype below: function handleResponseFromAPI(promise)
+ * Append three handlers to the function:
+ * When the Promise resolves, return an object with the following attributes:
+ * status: 200 body: 'success'
+ * When the Promise rejects, return an empty Error object
+ * For every resolution, log 'Got a response from the API' to the console
+ */
 
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => ({
+  promise.then(() => ({
     status: 200,
     body: 'success',
   }))
     .catch(() => new Error())
-    .finally(
-      console.log('Got a response from the API to the console'),
-    );
+    .finally(() => console.log('Got a response from the API'));
 }
