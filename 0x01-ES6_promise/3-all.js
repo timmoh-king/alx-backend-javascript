4b76a5277faac89e404979cd470adb81ace33823
timmoh-king <timothykariuki43@gmail.com>
2023-09-27 13:07:29 +0300

    3-all.js

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,17 @@
+/*
+* In this file, import uploadPhoto and createUser from utils.js
+* Knowing that the functions in utils.js return promises
+* use the prototype below to collectively resolve all promises
+* and log body firstName lastName to the console.
+* In the event of an error, log Signup system offline to the console
+*/
+
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      console.log(`${values[1].firstName} ${values[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
