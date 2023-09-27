9ccdea25fb16114b2aa982704743cd0eb4848898
timmoh-king <timothykariuki43@gmail.com>
2023-09-27 23:07:23 +0300

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -11,7 +11,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((response) => {
-      console.log(`${response[0].body}, ${response[1].firstName}, ${response[1].lastName}`);
+      console.log(`${response[0].body} ${response[1].firstName} ${response[1].lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
