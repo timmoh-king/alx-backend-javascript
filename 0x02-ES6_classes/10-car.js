b0c539ad0de97c077c0792463dab402871ac94ac
timmoh-king <timothykaruki43@gmail.com>
2023-10-02 12:03:19 +0300

    10-car.js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -5,41 +5,41 @@
 * add a method called cloneCar that should return a new object of the class
 */
 export default class Car {
-  constructor (brand, motor, color) {
+  constructor(brand, motor, color) {
     this.brand = brand;
     this.motor = motor;
     this.color = color;
   }
 
-  get brand () {
+  get brand() {
     return this._brand;
   }
 
-  set brand (value) {
+  set brand(value) {
     this._brand = value;
   }
 
-  get motor () {
+  get motor() {
     return this._motor;
   }
 
-  set motor (value) {
+  set motor(value) {
     this._motor = value;
   }
 
-  get color () {
+  get color() {
     return this._color;
   }
 
-  set color (value) {
+  set color(value) {
     this._color = value;
   }
 
-  static get [Symbol.species] () {
+  static get [Symbol.species]() {
     return this;
   }
 
-  cloneCar () {
+  cloneCar() {
     const Species = this.constructor[Symbol.species];
 
     return new Species();
