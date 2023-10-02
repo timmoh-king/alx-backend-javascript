e8979d59f0b19e8f2e04d1963ea11c2705f528f5
timmoh-king <timothykaruki43@gmail.com>
2023-10-02 20:58:04 +0300

    101-iterateThroughObject.js

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,7 +1,8 @@
 /*
- *  write a function named iterateThroughObject
- *  The function’s parameter reportWithIterator is the return value from createIteratorObject.
- *  It should return every employee name in a string, separated by |
+ * write a function named iterateThroughObject
+ * The function’s parameter reportWithIterator is the return value from createIteratorObject.
+ * It should return every employee name in a string, separated by |
+ * Reminder - the functions will be imported by the test suite.
  */
 
 export default function iterateThroughObject(reportWithIterator) {
