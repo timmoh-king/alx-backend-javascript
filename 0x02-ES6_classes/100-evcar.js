4dbed97ad4962e1048c55452a3b47da7b844aeef
timmoh-king <timothykaruki43@gmail.com>
2023-10-02 23:20:39 +0300

    100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -10,17 +10,20 @@
 import Car from './10-car';
 
 export default class EVCar extends Car {
-	constructor(brand, motor, color, range) {
-		super(brand, motor, color);
-		this._range = range;
-	}
-	get range() {
-		return this._range;
-	}
-	set range(value) {
-		this._range = value;
-	}
-	cloneCar() {
-		return new Car;
-	}
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range (value) {
+    this._range = value;
+  }
+
+  cloneCar() {
+    return new Car();
+  }
 }
