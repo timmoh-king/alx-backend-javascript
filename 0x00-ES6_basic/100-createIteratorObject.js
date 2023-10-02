b883c401928e5fa10b1657d474577c684dea187c
timmoh-king <timothykaruki43@gmail.com>
2023-10-02 20:31:59 +0300

    100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -4,10 +4,10 @@
  * This function will return an iterator to go through every employee in every department
  */
 
-export default function createIteratorObject (report) {
+export default function createIteratorObject(report) {
   return (
     {
-      * [Symbol.iterator] () {
+      * [Symbol.iterator]() {
         for (const department of Object.keys(report.allEmployees)) {
           for (const employee of report.allEmployees[department]) {
             yield employee;
