47941e01794c7c1df19bf2bf4f792097730e632c
timmoh-king <timothykaruki43@gmail.com>
2023-10-03 16:33:28 +0300

    0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -6,8 +6,8 @@
 
 export default function getListStudents() {
   return [
-    { firstName: 'Guillaume', id: 1, location: 'San Fransisco' },
+    { firstName: 'Guillaume', id: 1, location: 'San Francisco' },
     { firstName: 'James', id: 2, location: 'Columbia' },
-    { firstName: 'Serena', id: 5, location: 'San Fransisco' },
+    { firstName: 'Serena', id: 5, location: 'San Francisco' },
   ];
 }
