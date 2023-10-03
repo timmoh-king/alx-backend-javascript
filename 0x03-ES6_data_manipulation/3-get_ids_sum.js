5a67b1fd982f697532770241801095821286f82b
timmoh-king <timothykaruki43@gmail.com>
2023-10-03 19:50:39 +0300

    3-get_ids_sum.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -5,6 +5,5 @@
 
 export default function (studentObj) {
   const idList = Object.values(studentObj).map((o) => o.id);
-  console.log(idList)
   return idList.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 }
