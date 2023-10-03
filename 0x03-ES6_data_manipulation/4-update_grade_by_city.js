c594ee244c3cb6626f000deee1faff715992dda1
timmoh-king <timothykaruki43@gmail.com>
2023-10-03 22:52:30 +0300

    4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -4,11 +4,16 @@
  *   accept a list of students (from getListStudents), a city (String), and newGrades
  */
 
+
 export default function updateStudentGradeByCity(studentObj, city, newGrades) {
   const students = Object.values(studentObj).filter((o) => o.location === city);
+
   const updatedStudents = students.map((student) => ({
-    ...student,
-    grade: newGrades[student.id],
+    firstName: student.firstName,
+    id: student.id,
+    location: student.location,
+    grade: newGrades[student.id] ? newGrades[student.id].grade : 'N/A',
   }));
+
   return updatedStudents;
 }
