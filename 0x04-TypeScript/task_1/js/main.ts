46b607f2eeec8c03ad9173e48c5b8f0339926b9a
timmoh-king <timothykaruki43@gmail.com>
2023-10-04 19:58:04 +0300

    task_1: Let's build a Teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,16 @@
+interface Teacher {
+	firstName: string;
+	lastName: string;
+	fullTimeEmployee: boolean;
+	yearsOfExperience?: number;
+	location: string;
+	[key: string]: any;
+}
+const teacher3: Teacher = {
+	firstName: "John",
+	lastName: "Doe",
+	fullTimeEmployee: false,
+	location: "London",
+	contract: false,
+}
+console.log(teacher3)
