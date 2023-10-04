297b61ab11e487666734fd905ef0c83b75b8da93
timmoh-king <timothykaruki43@gmail.com>
2023-10-04 19:19:09 +0300

    main.ts: Creating an interface for a student

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,57 @@
+interface Student {
+	firstName: string;
+	lastName: string;
+	age: number;
+	location: string;
+}
+
+const student1: Student = {
+	firstName: 'Timothy',
+	lastName: 'Kariuki',
+	age: 23,
+	location: 'Nairobi',
+}
+const student2: Student = {
+	firstName: 'John',
+	lastName: 'Doe',
+	age: 27,
+	location: 'San Fransisco',
+}
+
+const studentsList: Student[] = [student1, student2];
+
+function createTable(): void {
+	const table = document.createElement('table');
+	const tableHead = document.createElement('thead');
+	const tableBody = document.createElement('tbody');
+
+	const headerRow = document.createElement('tr');
+	const firstNameHeader = document.createElement('th');
+	const locationHeader = document.createElement('th');
+
+	firstNameHeader.textContent = 'First Name';
+	locationHeader.textContent = 'Location';
+
+	headerRow.appendChild(firstNameHeader);
+	headerRow.appendChild(locationHeader);
+	tableHead.appendChild(headerRow);
+	table.appendChild(tableHead);
+
+	studentsList.forEach((student) => {
+		const row = document.createElement("tr");
+		const firstNameCell = document.createElement("td");
+		const locationCell = document.createElement("td");
+
+		firstNameCell.textContent = student.firstName;
+		locationCell.textContent = student.location;
+
+		row.appendChild(firstNameCell);
+		row.appendChild(locationCell);
+		tableBody.appendChild(row);
+	});
+
+	table.appendChild(tableBody);
+	document.body.appendChild(table);
+}
+
+createTable();
