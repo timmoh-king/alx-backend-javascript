7c93931d2749c25a21d37a8570171bd959cae00d
timmoh-king <timothykaruki43@gmail.com>
2023-10-05 06:25:01 +0300

    main.ts: String literal types

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -36,3 +36,21 @@ function createEmployee(salary: number | string): Director | Teacher {
 	}
 	return new Director;
 }
+
+//start of task 6
+interface Employee {
+	work(): string;
+}
+//end of task 6
+
+type Subjects = "Math" | "History";
+
+function teachClass(todayClass: Subjects): string {
+	if (todayClass === "Math") {
+		return "Teaching Math";
+	} else if (todayClass === "History") {
+		return "Teaching History";
+	} else {
+		return "Unknown subject";
+	}
+}
