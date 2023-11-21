fa5377978819c0573476aad7c3369855279efc79
timmoh-king <timothykariuki43@gmail.com>
2023-11-21 12:02:57 +0300

    6. Create a small HTTP server using Express

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,17 @@
+/**
+ * create a small HTTP server using Express module:
+ */
+
+const express = require('express');
+
+const app = express();
+const port = 1245;
+const host = '127.0.0.1';
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port, () => {
+  process.stdout.write(`Server running at http://${host}:${port}/`);
+});
