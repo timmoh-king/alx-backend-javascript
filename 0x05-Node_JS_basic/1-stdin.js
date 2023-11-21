46b9a9461b1170d0566f3a4c13ff102e1d1941da
timmoh-king <timothykariuki43@gmail.com>
2023-11-21 10:59:54 +0300

    1. Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,17 @@
-/*
+/**
 * Create a program named 1-stdin.js that will be executed through command line:
 */
 
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('data', () => {
+
+process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
 
   if (chunk) {
     process.stdout.write(`Your name is: ${chunk}`);
-    console.log(`chunk: ${chunk}`);
   }
 });
 
-process.stdout.on('end', () => {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
