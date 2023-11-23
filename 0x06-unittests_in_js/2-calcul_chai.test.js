2f6f342a4bedf2250142c510aa89f3cf57f8f7a9
timmoh-king <timothykariuki43@gmail.com>
2023-11-23 09:14:53 +0300

    Updated 2. Basic test using Chai assertion library

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -4,15 +4,19 @@ const calculateNumber = require('./1-calcul');
 
 describe('number test', () => {
   it('handle the sum type', () => {
-    expect(calculateNumber('SUM', 1.4, 4.5), 6);
+    const result = calculateNumber('SUM', 1.4, 4.5);
+    expect(result).to.equal(6);
   });
   it('handle the subtraction type', () => {
-    expect(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    const result = calculateNumber('SUBTRACT', 1.4, 4.5);
+    expect(result).to.equal(-4);
   });
   it('handles divide type', () => {
-    expect(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    const result = calculateNumber('DIVIDE', 1.4, 4.5);
+    expect(result).to.equal(0.2);
   });
   it('handles divide when b is 0', () => {
-    expect(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    const result = calculateNumber('DIVIDE', 1.4, 0);
+    expect(result).to.equal('Error');
   });
 });
