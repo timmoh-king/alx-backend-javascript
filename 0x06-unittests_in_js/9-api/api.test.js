32800b78b751651eefa229d023b8cb27938d326d
timmoh-king <timothykariuki43@gmail.com>
2023-11-23 13:01:56 +0300

    9. Regex integration testing

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,21 @@
+const chai = require('chai');
+const expect = chai.expect;
+const request = require('request');
+
+describe('app API test cases', () => {
+  const url = 'http://localhost:7865';
+
+  it('should return the correct response', (done) => {
+    request.get(`${url}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('should check the cart route', (done) => {
+    request.get(`${url}/`, (_err,res, body) => {
+      
+    })
+  })
+});
